0d0b3cb79dbaca2be66c8ed8749b9412d9388ae2
Rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2024-02-06 21:03:36 +0600

    Add project files.

---
+++ image data read/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tesseract;
+
+namespace imageDataRead
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            string imagePath = @"H:\\Download\\B R 2-min (1).jpg";
+            string text = ReadTextFromImage(imagePath);
+
+            Console.WriteLine("Text extracted from the image:");
+            Console.WriteLine(text);
+        }
+
+
+        static string ReadTextFromImage(string imagePath)
+        {
+            using (var engine = new TesseractEngine(@"G:\C#\image data read\image data read\tessdata\", "eng", EngineMode.Default))
+            {
+                using (var img = Pix.LoadFromFile(imagePath))
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        return page.GetText();
+                    }
+                }
+            }
+        }
+    }
+}

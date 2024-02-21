c14894dd469f88e7b2f01da4714d72eaa8d0019f
Rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2024-02-21 23:18:23 +0600

    update

--- image data read/Program.cs
+++ image data read/Program.cs
@@ -1,4 +1,7 @@
-﻿using Tesseract;
+﻿using System.IO;
+using System.Drawing;
+using Tesseract;
+using OpenCvSharp;
 
 namespace imageDataRead
 {
@@ -7,23 +10,41 @@ namespace imageDataRead
 
         static void Main()
         {
-            string imagePath = @"H:\\Download\\B R 2-min (1).jpg";
-            string text = ReadTextFromImage(imagePath);
+            var imagePath = "H:\\Download\\bg.png";
 
-            Console.WriteLine("Text extracted from the image:");
-            Console.WriteLine(text);
-        }
+            byte[] imageArray = File.ReadAllBytes(imagePath);
+            string base64Image = Convert.ToBase64String(imageArray);
 
+            byte[] bytes = Convert.FromBase64String(base64Image);
 
-        static string ReadTextFromImage(string imagePath)
-        {
-            using (var engine = new TesseractEngine(@"G:\C#\image data read\image data read\tessdata\", "eng", EngineMode.Default))
+            using (MemoryStream ms = new MemoryStream(bytes))
             {
-                using (var img = Pix.LoadFromFile(imagePath))
+                // Convert MemoryStream to Mat (OpenCV image format)
+                Mat image = Mat.FromStream(ms, ImreadModes.Color);
+
+                // Convert image to grayscale
+                Mat grayImage = new Mat();
+                Cv2.CvtColor(image, grayImage, ColorConversionCodes.BGR2GRAY);
+
+                // Apply threshold
+                Mat thresholdImage = new Mat();
+                Cv2.Threshold(grayImage, thresholdImage, 128, 255, ThresholdTypes.Binary);
+
+                // Save the thresholded image to a file
+                string thresholdImagePath = "thresholded_image.png";
+                thresholdImage.SaveImage(thresholdImagePath);
+
+                // Perform OCR on the saved image using Tesseract
+                using (var engine = new TesseractEngine(@"G:\Csharp\image data read\image data read\tessdata\", "eng", EngineMode.Default))
                 {
-                    using (var page = engine.Process(img))
+                    using (var img = Pix.LoadFromFile(thresholdImagePath))
                     {
-                        return page.GetText();
+                        using (var page = engine.Process(img))
+                        {
+                            string text = page.GetText();
+                            Console.WriteLine("OCR Result:");
+                            Console.WriteLine(text);
+                        }
                     }
                 }
             }
